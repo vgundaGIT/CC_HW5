93db658312c6900010a74fa187602dd35acf2773
Gunda Varun <vgunda2@hawk.iit.edu>
2020-04-06 22:16:48 -0500

    Basic options implemented

--- MySort.cpp
+++ MySort.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+//For string functions
+#include <cstring>
 
 //Do the phase 2
 //We need only one thread anyway
@@ -16,15 +18,17 @@
 #define INTERNAL_SORTING 0
 #define EXTERNAL_SORTING 1
 
+#define DEFAULT_SORT_THREADS 48
 
 class File{
     //This may not be the entire dataset
     char *data;
     int fd;
+    int *fileName;
     public:
-    File(int file_descriptor,int size){
+    File(char* fileName,int size){
+        fd = -1;
         data = (char*)malloc(size*sizeof(char));
-        fd = file_descriptor;  
     };
     int Open(){
 
@@ -62,11 +66,44 @@ void QuickSort(char *arr, int size){
 //See what all options you can have
 class Options{
     int numThreads;
+    char *fileName;
+    bool fileNameExists;
+    bool threadFlagExists;
     public:
-    Options(char* input){
+    Options(int argc, char* argv[]){
+        threadFlagExists = false;
         //Parse input and set the number of threads
         //Default value is 48 as of now
+        for (int i = 0; i < argc; i++)
+        {
+            if(i+1 != argc){
+                if(strcmp(argv[i],"-t") == 0){
+                    numThreads = std::stoi(argv[i+1]);
+                    threadFlagExists = true;
+                }
+                else if(strcmp(argv[i],"-F") == 0){
+                    fileName = (char*)malloc(sizeof(char)*sizeof(*argv[i+1]));
+                    fileName = argv[i+1];
+                    fileNameExists = true;
+                }
+            }
+        }
+        if(threadFlagExists == false){
+            numThreads = DEFAULT_SORT_THREADS;
+        }
+    }
+    int sanityCheck(){
+        if(fileNameExists == false){
+            return 0;
+        }
+        return 1;
+    }
+    int getNumThreads(){
+        return numThreads;
     } 
+    char* getFileName(){
+        return fileName;
+    }
 };
 
 class Controller{
@@ -92,7 +129,21 @@ int fileSize(char *fileName){
 
 }
 
-int main(int argv,char *argc[]){
+int main(int argc,char *argv[]){
     //Just have options and controller objects together
     //here
+    Options opt = Options(argc,argv);
+    //If sanity check fails, then all the expected inputs are not present
+    if(!opt.sanityCheck()){
+        std::cout<<"Incorrect Usage"<<std::endl;
+        std::cout<<"Usage: ./ms.out -F [filename] -t [number of threads]"<<std::endl;
+        exit(0);
+    };
+
+
+
+    //std::cout<<opt.getNumThreads()<<std::endl;
+    //std::cout<<opt.getFileName()<<std::endl;
+
+    return 0;
 }
